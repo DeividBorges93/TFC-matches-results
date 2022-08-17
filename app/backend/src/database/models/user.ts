fba3faad42f2cca455fc4c33ddadb58ba9125741
DeividBorges93 <deividmborges@gmail.com>
2022-08-17 18:44:45 -0300

    refactor(Model): :recycle: modify user.model
    make it clear that it is public and add other attributes to init.

--- app/backend/src/database/models/user.ts
+++ app/backend/src/database/models/user.ts
@@ -1,19 +1,37 @@
-import { STRING, Model } from 'sequelize';
+import { STRING, INTEGER, Model } from 'sequelize';
 import db from '.';
 
 class User extends Model {
-  id?: number;
-  username: string;
-  role: string;
-  email: string;
-  password: string;
+  public id?: number;
+  public username: string;
+  public role: string;
+  public email: string;
+  public password: string;
 }
 
 User.init({
-  username: STRING,
-  role: STRING,
-  email: STRING,
-  password: STRING,
+  id: {
+    primaryKey: true,
+    type: INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
 }, {
   underscored: true,
   sequelize: db,
