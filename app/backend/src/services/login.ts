07c0fb00b9e8ea976f8828289c599fe5136fe811
DeividBorges93 <deividmborges@gmail.com>
2022-08-22 18:08:38 -0300

    refactor(service): :recycle: improves login service
    refactors the loginService to receive the proper validations in the login method returning the token
    
    add method validate that calls decrypt method to JWT and returns user role

--- app/backend/src/services/login.ts
+++ app/backend/src/services/login.ts
@@ -1,11 +1,26 @@
-import IUser from '../interfaces/IUser';
 import ILogin from '../interfaces/ILogin';
 import UserModel from '../database/models/user';
+import IError from '../interfaces/IError';
+import Jwt from '../utils/jwt';
 
 export default class LoginService {
-  public login = async (login: ILogin): Promise<IUser> => {
-    const user = await UserModel.findOne({ where: { email: login.email } });
+  public login = async (login: ILogin): Promise<string | IError> => {
+    const result = await (UserModel.findOne({ where: { email: login.email } }));
+    const jwt = new Jwt();
 
-    return user as IUser;
+    if (!result) return { code: 401, message: 'Incorrect email or password' };
+
+    const token = jwt.encrypt({ email: result.email, id: result.id, role: result.role });
+
+    return token;
+  };
+
+  public validate = (authorization: string): string => {
+    const jwt = new Jwt();
+
+    const { role } = jwt.decrypt(authorization);
+    console.log(role, 'role');
+
+    return role;
   };
 }
