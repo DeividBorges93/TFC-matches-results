4cbb422232f1208d8100ef732f92a0bdd57b117e
DeividBorges93 <deividmborges@gmail.com>
2022-08-22 18:00:34 -0300

    refactor(error): :recycle: add return before res. And add console.log(error)

--- app/backend/src/middlewares/errorMiddleware.ts
+++ app/backend/src/middlewares/errorMiddleware.ts
@@ -2,7 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import IError from '../interfaces/IError';
 
 const errorMiddleware = (error: IError, _req: Request, res: Response, _next: NextFunction) => {
-  if (error.code) res.status(error.code).json({ message: error.message });
+  if (error.code) return res.status(error.code).json({ message: error.message });
+
+  console.error(error);
 
   return res.status(500).json(error.message);
 };
