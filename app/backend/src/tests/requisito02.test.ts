0063facb6e2599b9d8c288d3331b28017a088b15
DeividBorges93 <deividmborges@gmail.com>
2022-08-23 15:36:29 -0300

    perf(tests): :speech_balloon: add userMock and tokenMock

--- app/backend/src/tests/requisito02.test.ts
+++ app/backend/src/tests/requisito02.test.ts
@@ -15,3 +15,15 @@ chai.use(chaiHttp);
 const { expect } = chai;
 const jwt = new Jwt();
 
+const userMockData = {
+  id: 1,
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+  // senha: secret_admin
+};
+
+
+const tokenMock = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsImlkIjoxLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NjEyMDU3NDgsImV4cCI6MTY2MTI5MjE0OH0._cNv1Bti3_xFaWrsmvSwggMHbWUVaqz6XX-xEjI7kvc"
+
