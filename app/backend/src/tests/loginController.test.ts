6009ac5d14b5ebb291823eb183895ad2555b5e7d
DeividBorges93 <deividmborges@gmail.com>
2022-08-23 18:36:42 -0300

    refactor(tests): :recycle: add tokenMock and change the name of  'describe'

--- app/backend/src/tests/loginController.test.ts
+++ app/backend/src/tests/loginController.test.ts
@@ -23,116 +23,120 @@ const userMockData = {
 
 const userMockNull = null;
 
-describe('LoginController.login', () => {
+const tokenMock = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsImlkIjoxLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NjEyODg5ODEsImV4cCI6MTY2MTM3NTM4MX0.NcHdSrqWzBxC9Tbi_h0FlL044lg_8sThecGlAxUrTak';
+
+describe('Login route', () => {
   afterEach(() => {
     sinon.restore();
   });
-  
-  describe('When receiving valid data in the request of the route "/login"', () => {
-    beforeEach(() => {
-      const { stub } = sinon;
-
-      stub(User, 'findOne').resolves(userMockData as User);
-    })
-
-    it('status HTTP 200 OK', async () => {
-      const response = await chai.request(app).post('/login').send({
-        email: 'admin@admin.com',
-        password: 'secret_admin',
-      });
-
-      expect(response.status).to.be.equal(200);
-    });
-
-    it('return a token OK', async () => {
-      const response = await chai.request(app).post('/login').send({
-        email: 'admin@admin.com',
-        password: 'secret_admin',
-      });
-
-      expect(response.body).to.be.haveOwnProperty('token');
-      expect(response.body.token).to.be.a('string');
-    });
-  });
-
-  describe('When NOT receiving valid data in the request of the route "/login"', () => {
-    describe('if user does not exist', () => {
-      beforeEach(() => {
-        const { stub } = sinon;
-  
-        stub(User, 'findOne').resolves(userMockNull);
-      })
-  
-      it('status 401 Unauthorized', async () => {
-        const response = await chai.request(app).post('/login').send({
-          email: 'any-email',
-          password: 'any-hash',
-        });
-  
-        expect(response.status).to.be.equal(401);
-      });
-  
-      it('return a message "Incorrect email or password"', async () => {
-        const response = await chai.request(app).post('/login').send({
-          email: 'any-email',
-          password: 'any-hash',
-        });
-  
-        expect(response.body).to.be.haveOwnProperty('message');
-        expect(response.body.message).to.be.a('string');
-        expect(response.body.message).to.be.equal('Incorrect email or password');
-      })
-    })
-  
-    describe('if "email" does not exist', () => {
+  describe('LoginController.login', () => {
+    
+    describe('When receiving valid data in the request of the route "/login"', () => {
       beforeEach(() => {
         const { stub } = sinon;
   
         stub(User, 'findOne').resolves(userMockData as User);
       })
   
-      it('status 400 bad_request', async () => {
+      it('status HTTP 200 OK', async () => {
         const response = await chai.request(app).post('/login').send({
-          password: 'secret_admin',
-        });
-        
-        expect(response.status).to.be.equal(400);
-      })
-  
-      it('return a message "All fields must be filled"', async () => {
-        const response = await chai.request(app).post('/login').send({
+          email: 'admin@admin.com',
           password: 'secret_admin',
         });
   
-        expect(response.body).to.be.haveOwnProperty('message');
-        expect(response.body.message).to.be.a('string');
-        expect(response.body.message).to.be.equal('All fields must be filled');
-      })
-    })
+        expect(response.status).to.be.equal(200);
+      });
   
-    describe('if "password" does not exist', () => {
-      beforeEach(() => {
-        const { stub } = sinon;
-  
-        stub(User, 'findOne').resolves(userMockData as User);
-      })
-  
-      it('status 400 bad_request', async () => {
+      it('return a token OK', async () => {
         const response = await chai.request(app).post('/login').send({
           email: 'admin@admin.com',
+          password: 'secret_admin',
         });
-        
-        expect(response.status).to.be.equal(400);
+  
+        expect(response.body).to.be.haveOwnProperty('token');
+        expect(response.body.token).to.be.a('string');
+      });
+    });
+  
+    describe('When NOT receiving valid data in the request of the route "/login"', () => {
+      describe('if user does not exist', () => {
+        beforeEach(() => {
+          const { stub } = sinon;
+    
+          stub(User, 'findOne').resolves(userMockNull);
+        })
+    
+        it('status 401 Unauthorized', async () => {
+          const response = await chai.request(app).post('/login').send({
+            email: 'any-email',
+            password: 'any-hash',
+          });
+    
+          expect(response.status).to.be.equal(401);
+        });
+    
+        it('return a message "Incorrect email or password"', async () => {
+          const response = await chai.request(app).post('/login').send({
+            email: 'any-email',
+            password: 'any-hash',
+          });
+    
+          expect(response.body).to.be.haveOwnProperty('message');
+          expect(response.body.message).to.be.a('string');
+          expect(response.body.message).to.be.equal('Incorrect email or password');
+        })
       })
-  
-      it('return a message "All fields must be filled"', async () => {
-        const response = await chai.request(app).post('/login').send({
-          email: 'admin@admin.com',
-        });
-  
-        expect(response.body).to.be.haveOwnProperty('message');
-        expect(response.body.message).to.be.a('string');
-        expect(response.body.message).to.be.equal('All fields must be filled');
+    
+      describe('if "email" does not exist', () => {
+        beforeEach(() => {
+          const { stub } = sinon;
+    
+          stub(User, 'findOne').resolves(userMockData as User);
+        })
+    
+        it('status 400 bad_request', async () => {
+          const response = await chai.request(app).post('/login').send({
+            password: 'secret_admin',
+          });
+          
+          expect(response.status).to.be.equal(400);
+        })
+    
+        it('return a message "All fields must be filled"', async () => {
+          const response = await chai.request(app).post('/login').send({
+            password: 'secret_admin',
+          });
+    
+          expect(response.body).to.be.haveOwnProperty('message');
+          expect(response.body.message).to.be.a('string');
+          expect(response.body.message).to.be.equal('All fields must be filled');
+        })
+      })
+    
+      describe('if "password" does not exist', () => {
+        beforeEach(() => {
+          const { stub } = sinon;
+    
+          stub(User, 'findOne').resolves(userMockData as User);
+        })
+    
+        it('status 400 bad_request', async () => {
+          const response = await chai.request(app).post('/login').send({
+            email: 'admin@admin.com',
+          });
+          
+          expect(response.status).to.be.equal(400);
+        })
+    
+        it('return a message "All fields must be filled"', async () => {
+          const response = await chai.request(app).post('/login').send({
+            email: 'admin@admin.com',
+          });
+    
+          expect(response.body).to.be.haveOwnProperty('message');
+          expect(response.body.message).to.be.a('string');
+          expect(response.body.message).to.be.equal('All fields must be filled');
+        })
       })
     })
   })
