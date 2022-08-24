442a53b3645aee1453189e237d0df05b5ac2b904
DeividBorges93 <deividmborges@gmail.com>
2022-08-24 17:26:05 -0300

    build(tests): :wrench: create test from teamsController

---
+++ app/backend/src/tests/teamsController.test.ts
@@ -0,0 +1,12 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Team from '../database/models/team';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
