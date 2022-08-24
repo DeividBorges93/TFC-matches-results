e9e5c7ac3b31a2e4ceb6c785569b4510a30aa5a9
DeividBorges93 <deividmborges@gmail.com>
2022-08-24 17:30:42 -0300

    feat(tests): :sparkles: adds tests for success cases to findAll and findByPk calls

--- app/backend/src/schemas/login.ts
+++ app/backend/src/schemas/login.ts
@@ -1,20 +1,20 @@
-import { ObjectSchema } from 'joi';
-import * as Joi from 'joi';
+// import { ObjectSchema } from 'joi';
+// import * as Joi from 'joi';
 
-const loginSchemas: ObjectSchema = Joi.object({
-  email: Joi
-    .string()
-    .required()
-    .messages({
-      'string.empty': '400|All fields must be filled',
-    }),
-  password: Joi
-    .string()
-    .min(7)
-    .required()
-    .messages({
-      'string.empty': '400|All fields must be filled',
-    }),
-});
+// const loginSchemas: ObjectSchema = Joi.object({
+//   email: Joi
+//     .string()
+//     .required()
+//     .messages({
+//       'string.empty': '400|All fields must be filled',
+//     }),
+//   password: Joi
+//     .string()
+//     .min(7)
+//     .required()
+//     .messages({
+//       'string.empty': '400|All fields must be filled',
+//     }),
+// });
 
-export default loginSchemas;
+// export default loginSchemas;
