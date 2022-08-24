b1ab99217a1cf7cf3bd03bbcd1263220e9460993
DeividBorges93 <deividmborges@gmail.com>
2022-08-24 16:25:03 -0300

    feat(controller): :sparkles: calls the layer teamService.findAll and findByPk
    findall returns all teams
    
    findbypk return one team by id

---
+++ app/backend/src/controllers/teams.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import TeamService from '../services/teams';
+
+export default class TeamController {
+  constructor(private teamService = new TeamService()) {}
+
+  public findAll = async (req: Request, res: Response) => {
+    const teams = await this.teamService.findAll();
+
+    return res.status(200).json(teams);
+  };
+
+  public findByPk = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const team = await this.teamService.findByPk(Number(id));
+
+    return res.status(200).json(team);
+  };
+}
