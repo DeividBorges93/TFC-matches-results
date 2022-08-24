a77cd1d18e875ac9ca29774ff10bdadd8a028b28
DeividBorges93 <deividmborges@gmail.com>
2022-08-24 18:51:50 -0300

    feat(service): :sparkles: calls the layer MatchModel.findAll
    define include and exclude

---
+++ app/backend/src/services/matches.ts
@@ -0,0 +1,25 @@
+import IHomeTeams from '../interfaces/IHomeTeams';
+import MatchModel from '../database/models/match';
+import TeamModel from '../database/models/team';
+import IMatch from '../interfaces/IMatch';
+import IReturnFindAndCountAllTeam from '../interfaces/IReturnFindAndCOuntAllTeam';
+
+export default class MatchService {
+  private _matches: MatchModel[];
+  private _matcheCreated: IMatch;
+  private _result: number;
+  private _teams: IReturnFindAndCountAllTeam;
+  private _teamRankings: IHomeTeams[];
+  private _homeTeams: TeamModel[];
+
+  public findAll = async () => {
+    this._matches = await MatchModel.findAll({
+      include: [
+        { model: TeamModel, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: TeamModel, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
+
+    return this._matches;
+  };
+}
