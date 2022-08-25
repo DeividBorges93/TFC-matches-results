7952fac9d1a746c81669cd419ddd0e8585d89356
DeividBorges93 <deividmborges@gmail.com>
2022-08-25 16:07:28 -0300

    feat(controller): :sparkles: adds the possibility to change the value of inProgress

--- app/backend/src/controllers/matches.ts
+++ app/backend/src/controllers/matches.ts
@@ -4,8 +4,16 @@ import MatchesService from '../services/matches';
 export default class MatchController {
   constructor(private matchService = new MatchesService()) {}
 
-  public findAll = async (_req: Request, res: Response) => {
-    const matches = await this.matchService.findAll();
+  public findAll = async (req: Request, res: Response) => {
+    let matches;
+
+    const { inProgress } = req.query;
+
+    if (!inProgress) matches = await this.matchService.findAll();
+    else {
+      const progressTrue = inProgress === 'true';
+      matches = await this.matchService.isInProgress(progressTrue);
+    }
 
     return res.status(200).json(matches);
   };
