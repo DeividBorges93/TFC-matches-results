9c21fc2de7e26d83786ed51564d52bc5cd0da57e
DeividBorges93 <deividmborges@gmail.com>
2022-08-29 18:14:15 -0300

    feat(interface): :sparkles: add homeTeamGoals and awayTeamGoals

--- app/backend/src/interfaces/ITeam.ts
+++ app/backend/src/interfaces/ITeam.ts
@@ -1,4 +1,8 @@
+import IGoals from './IGoals';
+
 export default interface ITeam {
   id?: number,
-  teamName: string
+  teamName: string,
+  homeTeamMatches?: IGoals[]
+  awayTeamMatches?: IGoals[]
 }
